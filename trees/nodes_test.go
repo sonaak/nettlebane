b8ddc138c2d11d20e381495daa24599bd31063ac
Knight Fu <knight.w.fu@gmail.com>
2019-01-20 00:56:17 -0500

    adds tests for bin and node; 100% test cov

--- trees/nodes_test.go
+++ trees/nodes_test.go
@@ -13,8 +13,70 @@ func TestNodes(t *testing.T) {
 
 var _ = Describe("Node", func() {
 	Describe("Node", func() {
-		It("should return the appropriate value", func() {
-			Expect(3).To(Equal(3))
+		Context("NewNode", func(){
+			It("should return the right value and children", func() {
+				nodes := make([]*Node, 0)
+				value := String("hello")
+				node := NewNode(value, nodes)
+				Expect(node.value).To(Equal(value))
+				Expect(node.childNodes).To(Equal(nodes))
+			})
+		})
+		Context("Value", func() {
+			It("should return the correct value", func(){
+				nodes := make([]*Node, 0)
+				value := String("hello")
+				node := NewNode(value, nodes)
+				Expect(node.Value()).To(Equal(value))
+			})
+		})
+
+		Context("Children", func() {
+			It("should return the correct Children", func(){
+				nodes := make([]*Node, 0)
+				value := String("hello")
+				node := NewNode(value, nodes)
+				Expect(node.Children()).To(Equal(nodes))
+			})
+		})
+	})
+
+	Describe("BinaryNode", func() {
+		Context("NewBinaryNode", func() {
+			It("should construct the right value and children", func(){
+				leftNode, rightNode := BinaryNode(Node{}), BinaryNode(Node{})
+				node := NewBinaryNode(String("hello"), &leftNode, &rightNode)
+
+				Expect((*Node)(node).Value()).Should(Equal(String("hello")))
+				Expect((*Node)(node).Children()).Should(ConsistOf(
+					(*Node)(&leftNode),
+					(*Node)(&rightNode),
+				))
+			})
+		})
+
+		Context("Comparer", func() {
+			It("should return the right value", func(){
+				leftNode, rightNode := BinaryNode(Node{}), BinaryNode(Node{})
+				node := NewBinaryNode(String("hello"), &leftNode, &rightNode)
+				Expect(node.Comparer()).Should(Equal(String("hello")))
+			})
+		})
+
+		Context("Left", func() {
+			It("should return the correct left node", func(){
+				leftNode, rightNode := BinaryNode(Node{}), BinaryNode(Node{})
+				node := NewBinaryNode(String("hello"), &leftNode, &rightNode)
+				Expect(node.Left()).Should(Equal(&leftNode))
+			})
+		})
+
+		Context("Right", func() {
+			It("should return the correct right node", func(){
+				leftNode, rightNode := BinaryNode(Node{}), BinaryNode(Node{})
+				node := NewBinaryNode(String("hello"), &leftNode, &rightNode)
+				Expect(node.Right()).Should(Equal(&rightNode))
+			})
 		})
 	})
 })
