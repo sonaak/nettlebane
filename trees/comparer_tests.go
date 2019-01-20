cc4bd68e709f92bb9dd05612bfa372ee3a224600
Knight Fu <knight.w.fu@gmail.com>
2019-01-19 23:27:45 -0500

    adds more test for comparer

--- trees/comparer_tests.go
+++ trees/comparer_tests.go
@@ -19,5 +19,15 @@ var _ = Describe("Comparers", func() {
 			Expect(comparable).To(BeTrue())
 			Expect(same).To(BeTrue())
 		})
+
+		It("should be less another Int64 of greater value", func() {
+			result := Int64(10).Compare(Int64(12))
+			Expect(result).To(Equal(LESSER))
+		})
+
+		It("should be less another Int64 of greater value", func() {
+			result := Int64(13).Compare(Int64(12))
+			Expect(result).To(Equal(GREATER))
+		})
 	})
 })
