0a5221daf2de8d1e831846884c6f1dcdab3a9d9e
Knight Fu <knight.w.fu@gmail.com>
2019-01-21 16:54:34 -0500

    adds basic tests for heap order

--- sorting/heap.go
+++ sorting/heap.go
@@ -1,1 +1,60 @@
 package sorting
+
+
+import (
+	. "github.com/sonaak/nettlebane/interfaces"
+)
+
+
+func left(index uint64) uint64 {
+	return index << 1 | 1
+}
+
+func right(index uint64) uint64 {
+	return (index + 1) << 1
+}
+
+func parent(index uint64) uint64 {
+	return (index - 1) >> 1
+}
+
+// A max binary heap
+// See Chapter 6 in Cormen et al. 2e (p. 127)
+type MaxHeap []Comparer
+
+func NewMaxHeap(comparers []Comparer) *MaxHeap {
+	return (*MaxHeap)(&comparers)
+}
+
+func (heap *MaxHeap) size() uint64 {
+	// nil heaps are empty
+	if heap == nil {
+		return 0
+	}
+
+	return uint64(len(([]Comparer)(*heap)))
+}
+
+func (heap *MaxHeap) order(index uint64) {
+	if heap == nil {
+		return
+	}
+
+	data := ([]Comparer)(*heap)
+	leftIndex, rightIndex := left(index), right(index)
+	largest := index
+	maxIndex := heap.size() - 1
+
+	if leftIndex <= maxIndex && data[largest].Compare(data[leftIndex]) == LESSER {
+		largest = leftIndex
+	}
+
+	if rightIndex <= maxIndex && data[largest].Compare(data[rightIndex]) == LESSER {
+		largest = rightIndex
+	}
+
+	if largest != index {
+		data[largest], data[index] = data[index], data[largest]
+		heap.order(largest)
+	}
+}
---
+++ sorting/heap_test.go
@@ -0,0 +1,86 @@
+package sorting
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/sonaak/nettlebane/interfaces"
+	"testing"
+)
+
+func TestHeaps(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Node Suite")
+}
+
+var _ = Describe("MaxHeaps", func() {
+	Context("NewMaxHeaps", func() {
+		It("should instantiate an empty list", func() {
+			comparers := []Comparer{}
+			Expect(func() {
+				NewMaxHeap(comparers)
+			}).ToNot(Panic())
+		})
+	})
+
+	Context("order", func() {
+		It("should order correctly", func() {
+		 	intList := []Comparer {
+		 		Int64(16),
+		 		Int64(4),
+		 		Int64(10),
+		 		Int64(14),
+		 		Int64(7),
+		 		Int64(9),
+		 		Int64(3),
+		 		Int64(2),
+		 		Int64(8),
+		 		Int64(1),
+			}
+			heap := MaxHeap(intList)
+			heap.order(1)
+
+			Expect(heap).To(Equal(MaxHeap([]Comparer {
+				Int64(16),
+		 		Int64(14),
+		 		Int64(10),
+		 		Int64(8),
+		 		Int64(7),
+		 		Int64(9),
+		 		Int64(3),
+		 		Int64(2),
+		 		Int64(4),
+		 		Int64(1),
+			})))
+		})
+
+		It("should stop when already ordered", func() {
+			intList := []Comparer {
+		 		Int64(16),
+		 		Int64(14),
+		 		Int64(10),
+		 		Int64(4),
+		 		Int64(7),
+		 		Int64(9),
+		 		Int64(3),
+		 		Int64(2),
+		 		Int64(8),
+		 		Int64(1),
+			}
+			heap := MaxHeap(intList)
+			heap.order(1)
+
+			Expect(heap).To(Equal(MaxHeap([]Comparer {
+				Int64(16),
+		 		Int64(14),
+		 		Int64(10),
+		 		Int64(4),
+		 		Int64(7),
+		 		Int64(9),
+		 		Int64(3),
+		 		Int64(2),
+		 		Int64(8),
+		 		Int64(1),
+			})))
+		})
+	})
+})
